96be847f030f554c9dbf603a58f028dfe451df6b
dvenkat <dhruv.venkat.2024@gmail.com>
2026-04-12 16:35:05 -0400

    bumped up grain

--- dhruvs-site/src/app/components/ambient-background.tsx
+++ dhruvs-site/src/app/components/ambient-background.tsx
@@ -6,7 +6,6 @@ export function AmbientBackground() {
     >
       <div className="ambient-glow ambient-glow-slate left-[-16rem] top-[-10rem] h-[34rem] w-[34rem] sm:h-[42rem] sm:w-[42rem]" />
       <div className="ambient-glow ambient-glow-amber bottom-[-16rem] right-[-12rem] h-[32rem] w-[32rem] sm:h-[40rem] sm:w-[40rem]" />
-      <div className="ambient-noise absolute inset-0" />
     </div>
   );
 }
